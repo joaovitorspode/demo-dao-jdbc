bae25417d51c2d1e1de6671e5bb7570011b335ef
João Vitor Spode <joaospode.contato@gmail.com>
2024-08-09 21:46:10 -0300

    findByDepartment

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,16 +3,26 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("===TEST 1: sell fndById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n===TEST 2: sell fndByDepartment ===");
+        Department department = new Department(2, null );
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
 
     }
 }
